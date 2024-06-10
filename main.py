c395868f088c04023bf84759c8334950be44c4b0
Cole Vondersitt <116603733+cvondersitt@users.noreply.github.com>
2024-06-10 19:08:26 +0000

    Added !gaming command functionality

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import discord
 import os
-import date
+import datetime
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -9,7 +9,9 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
-
+  myguild = client.guilds[0]
+  global events 
+  events = await myguild.fetch_scheduled_events()
 
 @client.event
 async def on_message(message):       
@@ -18,18 +20,19 @@ async def on_message(message):
 
   if message.content.startswith('!gaming'):
     eventExists = False
-    myguild = client.guilds[0]
-    for event in myguild._scheduled_events:
-      # ADD PARSER FOR DATE AND CHECK IF DATE IS THIS WEEK. IF EVENT EXISTS FOR THIS WEEK/NOT         CANCELLED, WE GAMING BOYS
-      
-    if eventExists:
-      await message.channel.send('We are not gaming this Sunday') 
-    else:
-      await message.channel.send('We gaming boys')
-      
-token = os.getenv('TOKEN'
+    for event in events:
+      monthOfEvent = 100 * event.start_time.month
+      dayOfEvent = event.start_time.day
+      dateEvent = monthOfEvent + dayOfEvent
+      dayNow = datetime.date.today().day
+      monthNow = 100 * datetime.date.today().month
+      dateNow = dayNow + monthNow
+      if dateNow >= (dateEvent - 7) and dateNow <= (dateEvent + 7) and event.status != 4:
+        m = int(monthOfEvent / 100) 
+        d = dayOfEvent
+        await message.channel.send('We are gaming Sunday ' + str(m) + '/' + str(d))
+      else:
+        await message.channel.send('We are not gaming this Sunday')
 
-if token is None:
-  print('Please set the TOKEN environment variable.')
-  exit(1)
+token = os.getenv('TOKEN')
 client.run(token)
